1ec7367bd2b84d9ff236fad021caeb23b37d447e
xlpacman805 <xlpacman805@gmail.com>
2024-03-09 03:47:26 -0600

    Update app prompt

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ configDotenv();
 const App = async (): Promise<string> => {
     const BASE_URL = process.env.ANTHROPIC_BASE_URL;
     const API_KEY = process.env.ANTHROPIC_API_KEY;
-    const message = 'Tell what makes Anthropic AI models different than other offerings on the market.';
+    const message = 'What\'s special about the Claude AI model?';
     const client = new AnthropicClient(BASE_URL, API_KEY, { anthropicVersion: '2023-06-01' });
     const response = await client.createMessage('claude-3-opus-20240229', [{
         content: message,
         role: 'user'
-    }], 1024);
+    }], 500, { temperature: 0.5 });
     console.log(response.content[0].text);
     return response.content[0].text;
 };
