9b7bb1b557169404bb9fa8a2e22a0a9a995c0ce1
xlpacman805 <xlpacman805@gmail.com>
2024-03-08 23:56:09 -0600

    add a simple unit test

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import { configDotenv } from 'dotenv';
 configDotenv();
 
-const STR = process.env.MY_STR;
-const message = 'hello world';
+const App = (): string => {
+    const STR = process.env.MY_STR;
+    const message = 'hello world';
 
-console.log(message);
-console.log(STR);
+    console.log(message);
+    console.log(STR);
+    return message;
+};
+
+export default App;
+App();
